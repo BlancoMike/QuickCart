151b4f35241e8293227a86d2f3201efbc0bc118e
BlancoMike <odetundemike@gmail.com>
2025-03-02 13:45:03 -0500

    Hopeful one

--- inngest/client.js
+++ inngest/client.js
@@ -1,3 +1,6 @@
 import { Inngest } from "inngest";
 
-export const inngest = new Inngest({ id: "quickcart-next" });
+export const inngest = new Inngest({
+  name: "QuickCart",
+  eventKey: process.env.INNGEST_EVENT_KEY,
+});
