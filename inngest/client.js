0efda5e933905c37015f39c4763dda232a8b73df
BlancoMike <odetundemike@gmail.com>
2025-03-06 19:46:10 -0500

    ID added

--- inngest/client.js
+++ inngest/client.js
@@ -2,5 +2,6 @@ import { Inngest } from "inngest";
 
 export const inngest = new Inngest({
   name: "QuickCart",
+  id: "quickcart-app", // Add a unique ID for your app
   eventKey: process.env.INNGEST_EVENT_KEY,
 });
